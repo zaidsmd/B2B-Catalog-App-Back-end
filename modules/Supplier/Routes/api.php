2c5aad193a59b393139c9d574bf6fd8bee529221
zaidsmd <zaidsmd111@gmail.com>
2025-12-25 23:51:59 +0100

    Supplier module: update middleware definition in API routes

--- modules/Supplier/Routes/api.php
+++ modules/Supplier/Routes/api.php
@@ -5,7 +5,7 @@ use Modules\Supplier\Http\Controllers\SupplierController;
 
 // API routes for module: Supplier
 Route::middleware('api')->prefix('api')->group(function () {
-    Route::middleware('auth:sanctum')->group( function () {
+    Route::middleware(['auth:sanctum'])->group( function () {
         Route::prefix('admin/v1')->name('admin.')->group(function () {
             Route::get('suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
             Route::get('suppliers/{supplier}/edit', [SupplierController::class, 'edit'])->name('suppliers.edit');
